c4e3030d5dab355cb866e4e1c85fac1abfa71ed2
Mike <mo.novichonok@gmail.com>
2024-05-31 10:15:53 -0400

    update

--- assets/characters.py
+++ assets/characters.py
@@ -1,34 +1,56 @@
-from typing import Literal
+from typing import Literal, Self
 from assets.weapons import Weapon, fists
-from assets.constants import MAP
+from assets.constants import MAP as map, STARTING_ROOM
 
 class Character:
     max_health: int = 40
-    default_damage: int = 5
     def __init__(self, weapon: Weapon) -> None:
         self.current_health: int = self.max_health
         self.type = "character"
         self.weapon: Weapon = weapon
         self.damage = weapon.damage
+        self.current_room: str = STARTING_ROOM
 
     def heal(self, heal_amount: int):
+        """ function created to heal current character
+
+        :param damage: int - number of damage to deal
+        :return: None
+        
+        Updates current hp for the character
+        """
         self.current_health = min(self.current_health+heal_amount, self.max_health)
 
     def get_damage(self, damage: int) -> None:
+        """ function created to get damage from characters
+
+        :param damage: int - number of damage to deal
+        :return: None
+        
+        Updates current hp for the character
+        """
         self.current_health = max(self.current_health - damage, 0)
 
     def deal_damage(self, opponent) -> None:
+        """ function created to deal damage to other entities
+        :param opponent - other entity
+        :return: None - deals damage to other entity
+        """
         opponent.get_damage(self.damage)
 
-    def move(self, direction: Literal["forward", "left", "right"]) -> NotImplementedError:
-        raise NotImplementedError("Add ability to move between rooms")
-
+    def move(self, direction: Literal["forward", "left", "right"]) -> ValueError | None:
+        ## cheks proper value was passed in param: direction
+        if direction not in ["forward", "left", "right"]:
+            raise ValueError("incorrect direction")
+        ## checks if player can go to this direction from current room
+        if direction not in map[self.current_room]:
+            raise ValueError("Can't go this direction from current room")
+        self.current_room = map[self.current_room][direction]
 
 class Hero(Character):
     def __init__(self, weapon: Weapon = fists) -> None:
         super().__init__(weapon=weapon)
         self.type = "player"
-        # self.damage = Character.default_damage
 
     def __str__(self) -> str:
         return f"Class: {self.type}\n--Current Health: {self.current_health}\n--Damage: {self.damage}"
--- assets/constants.py
+++ assets/constants.py
@@ -1,5 +1,10 @@
+# dictionary to put a map inside
 MAP: dict[str, dict[str, str]] = {}
 
+# variable for starting room
+STARTING_ROOM: str = None
+
+# dictionary with drarity drop chances
 RARITY_CHANCES: dict[str, int | float] = {
     "rare": 25
     , "epic": 12
